363f7ae4253b8041f9506cbbd2c8293ab45b2ed0
James Laver <james@trustrobots.com>
2014-01-10 14:56:50 +0000

    Depend on flatland.ordered.map

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :repl-options {:init-ns mapmapper.repl}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [korma "0.3.0-RC6"]
+                 [org.flatland/ordered "1.5.2"]
                  [com.taoensso/timbre "2.7.1"]]
   :plugins [[lein-midje "3.1.3"]]
   :profiles {:dev {:dependencies [[midje "1.6.0"]]}})
